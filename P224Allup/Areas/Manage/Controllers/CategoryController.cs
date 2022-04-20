10ab3783724690b16e61983c4cb5bbaac3f342dc
HamidMammadov <hamidvm@code.edu.az>
2022-04-20 18:18:22 +0400

    First Commit

---
+++ P224Allup/Areas/Manage/Controllers/CategoryController.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using P224Allup.DAL;
+using P224Allup.Extensions;
+using P224Allup.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace P224Allup.Areas.Manage.Controllers
+{
+    [Area("manage")]
+    public class CategoryController : Controller
+    {
+        private readonly AllupDbContext _context;
+        private readonly IWebHostEnvironment _env;
+
+
+        public CategoryController(AllupDbContext context, IWebHostEnvironment env)
+        {
+            _context = context;
+            _env = env;
+        }
+
+        public async Task<IActionResult> Index(bool? status, int page = 1)
+        {
+            ViewBag.Status = status;
+
+            IEnumerable<Category> categories = await _context.Categories
+                .Include(t => t.Products)
+                .Where(t => status != null ? t.IsDeleted == status : true)
+                .OrderByDescending(t => t.CreatedAt)
+                .ToListAsync();
+
+            ViewBag.PageIndex = page;
+            ViewBag.PageCount = Math.Ceiling((double)categories.Count() / 5);
+
+            return View(categories.Skip((page - 1) * 5).Take(5));
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            ViewBag.MainCategory = await _context.Categories.Where(c => c.IsMain && !c.IsDeleted).ToListAsync();
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Category category)
+        {
+            ViewBag.MainCategory = await _context.Categories.Where(c => c.IsMain && !c.IsDeleted).ToListAsync();
+
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            if (string.IsNullOrWhiteSpace(category.Name))
+            {
+                ModelState.AddModelError("Name", "Bosluq Olmamalidir");
+                return View();
+            }
+
+            //tag.Name = tag.Name.Trim();
+
+            if (category.Name.CheckString())
+            {
+                ModelState.AddModelError("Name", "Yalniz Herf Ola Biler");
+                return View();
+            }
+
+            if (await _context.Categories.AnyAsync(t => t.Name.ToLower() == category.Name.ToLower()))
+            {
+                ModelState.AddModelError("Name", "Alreade Exists");
+                return View();
+            }
+
+            if (category.IsMain)
+            {
+                category.ParentId = null;
+
+                if (category.CategoryImage == null)
+                {
+                    ModelState.AddModelError("CategoryImage", "Sekil Mutleq Olmalidi");
+                    return View();
+                }
+
+                if (!category.CategoryImage.CheckFileContentType("image/jpeg"))
+                {
+                    ModelState.AddModelError("LogoImage", "Secilen Seklin Novu Uygun");
+                    return View();
+                }
+
+                if (!category.CategoryImage.CheckFileSize(30))
+                {
+                    ModelState.AddModelError("LogoImage", "Secilen Seklin Olcusu Maksimum 30 Kb Ola Biler");
+                    return View();
+                }
+
+                category.Image = category.CategoryImage.CreateFile(_env, "assets", "images");
+            }
+            else
+            {
+                if (category.ParentId == null)
+                {
+                    ModelState.AddModelError("ParentId", "Parent Mutleq Secilmelidir");
+                    return View();
+                }
+
+                if (!await _context.Categories.AnyAsync(c=>c.Id == category.ParentId && !c.IsDeleted && c.IsMain))
+                {
+                    ModelState.AddModelError("ParentId", "Parent Id yanlisdir");
+                    return View();
+                }
+            }
+
+            category.CreatedAt = DateTime.UtcNow.AddHours(4);
+
+            await _context.Categories.AddAsync(category);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ P224Allup/Models/Product.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace P224Allup.Models
+{
+    public class Product : BaseEntity
+    {
+        [StringLength(255),Required]
+        public string Title { get; set; }
+        [Column(TypeName ="money")]
+        public double DiscountPrice { get; set; }
+        [Column(TypeName = "money")]
+        public double Price { get; set; }
+        [Column(TypeName = "money")]
+        public double ExTax { get; set; }
+        [StringLength(8)]
+        public string Code { get; set; }
+        [StringLength(1000)]
+        public string Description { get; set; }
+        public int Count { get; set; }
+        public string MainImage { get; set; }
+        public string HoverImage { get; set; }
+        public bool Availability { get; set; }
+
+        public Nullable<int> BrandId { get; set; }
+        public Brand Brand { get; set; }
+        public Nullable<int> CategoryId { get; set; }
+        public Category Category { get; set; }
+        public IEnumerable<ProductTag> ProductTags { get; set; }
+        public IEnumerable<ProductImage> ProductImages { get; set; }
+        public IEnumerable<Review> Reviews { get; set; }
+    }
+}
